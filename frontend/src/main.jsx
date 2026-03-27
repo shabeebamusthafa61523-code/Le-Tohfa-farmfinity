557eb2179de51c4f544f0598943f085a72392b03
shabeebamusthafa61523-code <shabeebamusthafa61523@gmail.com>
2026-03-27 10:32:09 +0530

    updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './App.css' 
+import './app.css' 
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import axios from 'axios'
