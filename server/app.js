7628352e65076a0cd8e0d4ccfab6228cdb293684
shabeebamusthafa61523-code <shabeebamusthafa61523@gmail.com>
2026-03-28 06:08:21 -0500

    updates

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ connectDB();
 // 2. REFINED CORS CONFIGURATION
 const allowedOrigins = [
     "http://localhost:5173",
-       "https://le-tohfa-farmfinity.vercel.app/"
+       "https://le-tohfa-farmfinity.vercel.app/",
 
     "https://shabeebamusthafa61523-code-farmfini-rho.vercel.app",
     "https://shabeebamusthafa61523-code-letoh-qa.vercel.app",
